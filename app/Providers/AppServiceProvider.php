ce7701516a87a4724774d730c66d81f58ad24e9b
Anita Rodney <anotarodney30@gmail.com>
2025-11-11 19:30:16 +0100

    templateservice

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton('auth.token', function ($app) {
+            return new \App\Http\Middleware\RequestToken();
+        });
+
     }
 
     /**
